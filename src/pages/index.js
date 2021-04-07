e4917cb8685d7db93c47c624ac73feea241aef32
Matt Davenport <matt@matthewdavenport.com.au>
2021-04-08 09:36:55 +1000

    SEO data

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import App from '../components/App';
 import { headData } from '../mock/data';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/main.scss';
+import SEO from '../components/seo';
 
 export default () => {
   const { title, lang, description } = headData;
@@ -15,6 +16,7 @@ export default () => {
         <title>{title || 'Matthew Davenport Portfolio'}</title>
         <html lang={lang || 'en'} />
         <meta name="description" content={description || 'Matthew Davenport Portfolio'} />
+        <SEO image="headshot.jpeg" />
       </Helmet>
       <App />
     </>
