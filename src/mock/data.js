01a9f115c39eacfcb0bbdb5cb3b9465565a34eee
Matt Davenport <matt@matthewdavenport.com.au>
2021-04-07 14:51:26 +1000

    Images and bio

--- src/mock/data.js
+++ src/mock/data.js
@@ -17,11 +17,14 @@ export const heroData = {
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'headshot.jpeg',
+  paragraphOne:
+    'Hi, my name is Matthew Davenport. My expertise lies in User Experience Design, this includes prototyping, wireframes, front-end design, research and general design. Apart from technical digital work, I have a keen interest working with others. I have experience in customer service, administration and general office duties.',
+  paragraphTwo:
+    'I have a general coding know which includes both programming and web developement in multiple languages. I am proficient in using standard CMS platforms such as Wordpress and Squarespace, the Adobe Suite, filming, video editing, designing and creating graphics, and creating digital content.',
+  paragraphThree:
+    'I also have experience growing large social media audiences and executing digital campaign strategies. I have experience in creating and sending email campaigns to over 1 Million individuals, and serving digital ads and content to similar audiences.',
+  resume: 'https://www.linkedin.com/in/matthew-davenport-b536a210b/', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -78,12 +81,12 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/matthew-davenport-b536a210b/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/mattdav30',
     },
   ],
 };
