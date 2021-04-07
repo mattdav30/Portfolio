802a408d0559fad370c580f1eb80855751625ac0
Matt Davenport <matt@matthewdavenport.com.au>
2021-04-07 14:46:14 +1000

    new

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,15 +2,15 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Matthew Davenport | Portfolio', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Portfolio showcasing my current and past work.', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
+  name: 'Matthew Davenport',
   subtitle: '',
   cta: '',
 };
